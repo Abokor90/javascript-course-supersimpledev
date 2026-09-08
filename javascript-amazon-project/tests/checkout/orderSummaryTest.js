07f4799ac769aa77b08f97b2a4fe5b4535998e92
Abokor90 <abdikania163@gmail.com>
2026-09-08 10:34:18 +0300

    Load products in beforeAll hook

--- javascript-amazon-project/tests/checkout/orderSummaryTest.js
+++ javascript-amazon-project/tests/checkout/orderSummaryTest.js
@@ -1,10 +1,17 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
+import { loadProduct } from "../../data/products.js";
 
  
 describe('test suite: renderOrderSummary', () => {
   const productId1 = '83d4ca15-0f35-48f5-b7a3-1ea210004f2e';
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
+
+  beforeAll((done) => {
+    loadProduct(() => {
+      done();
+    });
+  });
 
   beforeEach(() => {
     spyOn(localStorage, 'setItem');
